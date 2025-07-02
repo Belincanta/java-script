617642163c05d09a3e65d5a53c42abbd919b9773
Alessandro Belincanta <belincantaalessandro@gmail.com>
2025-07-01 21:40:03 -0300

    Trabalhando com repetições simples

---
+++ aula13/testes.js
@@ -0,0 +1,15 @@
+    
+/*Criando uma repetição para testar no inicio, enquanto for verdadeiro vai executar
+var c = 1
+while(c <=5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+//Criando uma repetição para testar no final//
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while(c <=5)
