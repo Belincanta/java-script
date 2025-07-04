5ddb509269f203774d61efe70262ef72368e2fbf
Alessandro Belincanta <belincantaalessandro@gmail.com>
2025-07-03 22:08:55 -0300

    Exercicios

---
+++ ex003/script.js
@@ -0,0 +1,7 @@
+var inicio = document.getElementById('txinicio')
+var fim = document.getElementById('txfim')
+var pular = document.getElementById('txpasso')
+var res = document.getElementById('resultado')
+var soma = inicio + fim
+var res = soma
+
